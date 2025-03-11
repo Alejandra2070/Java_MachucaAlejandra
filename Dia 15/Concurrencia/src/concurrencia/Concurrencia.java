9838815e24377345317d2320e90d35227f6d3c26
Alejandra2070 <am3793825@gmail.com>
2025-03-11 12:49:50 -0500

    :sparkles: subject seen in class

---
+++ Dia 15/Concurrencia/src/concurrencia/Concurrencia.java
@@ -0,0 +1,9 @@
+package concurrencia;
+
+public class Concurrencia {
+
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
---
+++ Dia 15/PatronesDiseno_Comportamiento/src/patronesdiseno_comportamiento/PatronesDiseno_Comportamiento.java
@@ -0,0 +1,17 @@
+package patronesdiseno_comportamiento;
+
+public class PatronesDiseno_Comportamiento {
+
+    //Patrones de comportamiento
+    
+    //Son patrones que se enfocan en cómo las clases y objetos interactúan y se comunican entre sí
+    //para lograr diferentes comportamientos y funcionalidades. Estas proporcionarán soluciones para gestionar
+    //la colaboración y coordinación entre las clases.
+    
+    //Formalizaremos los siguientes patrones:
+    
+    //1. Chain of Command (Patrón de Comando)
+    //2. Iterator
+    //3. Mediator
+    
+}
